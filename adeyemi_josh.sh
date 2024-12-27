9c1a5c9978448fe99c11d9a92ce4d9466b1cf767
Ubuntu <ubuntu@ip-172-31-24-161.ec2.internal>
2024-12-27 18:00:22 +0000

    Bash script assignment Joshua Adeyemi

---
+++ adeyemi_josh.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am Joshua Adeyemi"
+}
+
+print_fullname() {
+  echo "Full Name: Adeyemi Joshua Oluwajuwon"
+}
+
+print_bio() {
+  echo "Bio: I am a Cloud/DevOps Engineer with experience in building and maintaining scalable infrastructure and deployment pipelines. I have strong problem-solving skills, pay attention to detail, and adapt quickly to new tools and technologies. I am passionate about streamlining processes and ensuring efficient collaboration across teams."
+}
+
+print_year_in_tech() {
+  echo "Year in Tech: I have been in tech for 9 months learning about the tools in Cloud Engineering so as to best."
+}
+
+print_dream_job() {
+  echo "Dream Job: I would love to work at Microsoft as a DevOps Engineer for like 4 years, gaining invaluable experience in managing large-scale infrastructure and contributing to innovative projects. Afterward, I aspire to work with AWS, leveraging their cutting-edge cloud technologies to design and implement robust solutions for businesses worldwide."
+}
+
+print_goals() {
+  echo "Goals ⚽: I would love to use my field to create jobs, help in the development of technology that could sustain lives, and build a sustainable country. Additionally, I aim to make a lot of money to provide for my family and friends while making a positive impact in the world."
